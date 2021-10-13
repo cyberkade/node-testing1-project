27283df358ea9da870f9cde64c4fb69eacf75ba6
Kade Griffith <kadeg95@gmail.com>
2021-10-13 14:25:56 -0600

    completed all tests

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-/**
- * [Exercise 1] trimProperties copies an object trimming its properties
- * @param {object} obj - an object with properties that are strings
- * @returns {object} - a copy of the object with strings trimmed
- *
- * EXAMPLE
- * trimProperties({ name: '  jane  ' }) // returns a new object { name: 'jane' }
- */
 function trimProperties(obj) {
   const trimmed = JSON.stringify(obj, (key, value) => {
     if (typeof value === "string") {
@@ -26,125 +18,71 @@ function trimProperties(obj) {
   // );
 }
 
-/**
- * [Exercise 2] trimPropertiesMutation trims in place the properties of an object
- * @param {object} obj - an object with properties that are strings
- * @returns {object} - the same object with strings trimmed
- *
- * EXAMPLE
- * trimPropertiesMutation({ name: '  jane  ' }) // returns the object mutated in place { name: 'jane' }
- */
 function trimPropertiesMutation(obj) {
   Object.keys(obj).map((k) => (obj[k] = obj[k].trim()));
   return obj;
 }
 
-/**
- * [Exercise 3] findLargestInteger finds the largest integer in an array of objects { integer: 1 }
- * @param {object[]} integers - an array of objects
- * @returns {number} - the largest integer
- *
- * EXAMPLE
- * findLargestInteger([{ integer: 1 }, { integer: 3 }, { integer: 2 }]) // returns 3
- */
 function findLargestInteger(integers) {
-  integers.reduce();
+  const largest = integers.reduce((acc, res) => {
+    return acc.integer > res.integer ? acc : res;
+  });
+  return largest.integer;
 }
 
 class Counter {
-  /**
-   * [Exercise 4A] Counter creates a counter
-   * @param {number} initialNumber - the initial state of the count
-   */
   constructor(initialNumber) {
-    // ✨ initialize whatever properties are needed
+    this.count = initialNumber;
   }
-
-  /**
-   * [Exercise 4B] Counter.prototype.countDown counts down to zero
-   * @returns {number} - the next count, does not go below zero
-   *
-   * EXAMPLE
-   * const counter = new Counter(3)
-   * counter.countDown() // returns 3
-   * counter.countDown() // returns 2
-   * counter.countDown() // returns 1
-   * counter.countDown() // returns 0
-   * counter.countDown() // returns 0
-   */
   countDown() {
-    // ✨ implement
+    if (this.count > 0) {
+      return this.count--;
+    }
+    return this.count;
   }
 }
 
 class Seasons {
-  /**
-   * [Exercise 5A] Seasons creates a seasons object
-   */
   constructor() {
-    // ✨ initialize whatever properties are needed
+    this.seasons = ["summer", "fall", "winter", "spring"];
+    this.currentSeason = 0;
   }
-
-  /**
-   * [Exercise 5B] Seasons.prototype.next returns the next season
-   * @returns {string} - the next season starting with "summer"
-   *
-   * EXAMPLE
-   * const seasons = new Seasons()
-   * seasons.next() // returns "summer"
-   * seasons.next() // returns "fall"
-   * seasons.next() // returns "winter"
-   * seasons.next() // returns "spring"
-   * seasons.next() // returns "summer"
-   */
   next() {
-    // ✨ implement
+    const result = this.seasons[this.currentSeason];
+    this.currentSeason === 3 ? (this.currentSeason = 0) : this.currentSeason++;
+    return result;
   }
 }
 
 class Car {
-  /**
-   * [Exercise 6A] Car creates a car object
-   * @param {string} name - the name of the car
-   * @param {number} tankSize - capacity of the gas tank in gallons
-   * @param {number} mpg - miles the car can drive per gallon of gas
-   */
   constructor(name, tankSize, mpg) {
-    this.odometer = 0; // car initilizes with zero miles
-    this.tank = tankSize; // car initiazes full of gas
-    // ✨ initialize whatever other properties are needed
+    this.odometer = 0;
+    this.tank = tankSize;
+    this.name = name;
+    this.mpg = mpg;
   }
-
-  /**
-   * [Exercise 6B] Car.prototype.drive adds miles to the odometer and consumes fuel according to mpg
-   * @param {string} distance - the distance we want the car to drive
-   * @returns {number} - the updated odometer value
-   *
-   * EXAMPLE
-   * const focus = new Car('focus', 20, 30)
-   * focus.drive(100) // returns 100
-   * focus.drive(100) // returns 200
-   * focus.drive(100) // returns 300
-   * focus.drive(200) // returns 500
-   * focus.drive(200) // returns 600 (ran out of gas after 100 miles)
-   */
   drive(distance) {
-    // ✨ implement
+    let gallonsUsed = Math.round(distance / this.mpg);
+    if (this.tank <= 0) {
+      return `You ran out of fuel at ${this.odometer} miles`;
+    } else if (this.tank < gallonsUsed) {
+      const leftOver = gallonsUsed - this.tank;
+      const milesDrove = distance - leftOver * this.mpg;
+      this.odometer += milesDrove;
+      this.tank = 0;
+      return `You ran out of gas while driving ${distance} miles! You made it to mile ${milesDrove}. Odometer is at ${this.odometer}`;
+    } else {
+      this.odometer += distance;
+      this.tank -= Math.round(gallonsUsed);
+      return this.odometer;
+    }
   }
-
-  /**
-   * [Exercise 6C] Adds gallons to the tank
-   * @param {number} gallons - the gallons of fuel we want to put in the tank
-   * @returns {number} - the miles that can be driven after refueling
-   *
-   * EXAMPLE
-   * const focus = new Car('focus', 20, 30)
-   * focus.drive(600) // returns 600
-   * focus.drive(1) // returns 600 (no distance driven as tank is empty)
-   * focus.refuel(99) // returns 600 (tank only holds 20)
-   */
   refuel(gallons) {
-    // ✨ implement
+    if (this.tank + gallons > 20) {
+      return "600 (tank only holds 20)";
+    }
+    this.tank += gallons;
+    return this.tank * this.mpg;
   }
 }
 
@@ -161,8 +99,8 @@ class Car {
  *    // result is false
  * })
  */
-function isEvenNumberAsync(number) {
-  // ✨ implement
+async function isEvenNumberAsync(number) {
+  return number % 2 === 0 ? true : false;
 }
 
 module.exports = {
--- index.test.js
+++ index.test.js
@@ -8,7 +8,6 @@ describe("[Exercise 1] trimProperties", () => {
     const actual = utils.trimProperties(input);
     expect(actual).toEqual(expected);
   });
-
   test("[2] returns a copy, leaving the original object intact", () => {
     const input = { name: "  jane  ", age: "21 ", status: "  married  " };
     const expected = { name: "jane", age: "21", status: "married" };
@@ -19,19 +18,15 @@ describe("[Exercise 1] trimProperties", () => {
 });
 
 describe("[Exercise 2] trimPropertiesMutation", () => {
-  // test('[3] returns an object with the properties trimmed', () => {})
-  // test('[4] the object returned is the exact same one we passed in', () => {})
   let user;
   beforeEach(() => {
     user = { name: "  jane  ", age: "21 ", status: "  married  " };
   });
-
   test("[3] returns an object with the properties trimmed", () => {
     const expected = { name: "jane", age: "21", status: "married" };
     const actual = utils.trimPropertiesMutation(user);
     expect(actual).toEqual(expected);
   });
-
   test("[4] the object returned is the exact same one we passed in", () => {
     const result = utils.trimPropertiesMutation(user);
     expect(result).toBe(user);
@@ -39,44 +34,112 @@ describe("[Exercise 2] trimPropertiesMutation", () => {
 });
 
 describe("[Exercise 3] findLargestInteger", () => {
-  // test('[5] returns the largest number in an array of objects { integer: 2 }', () => {})
+  test("[5] returns the largest number in an array of objects { integer: 2 }", () => {
+    const input = [{ integer: 1 }, { integer: 3 }, { integer: 2 }];
+    const actual = utils.findLargestInteger(input);
+    expect(actual).toBe(3);
+  });
 });
 
 describe("[Exercise 4] Counter", () => {
   let counter;
   beforeEach(() => {
-    counter = new utils.Counter(3); // each test must start with a fresh couter
+    counter = new utils.Counter(3);
   });
-  // test('[6] the FIRST CALL of counter.countDown returns the initial count', () => {})
-  // test('[7] the SECOND CALL of counter.countDown returns the initial count minus one', () => {})
-  // test('[8] the count eventually reaches zero but does not go below zero', () => {})
+  test("[6] the FIRST CALL of counter.countDown returns the initial count", () => {
+    expect(counter.countDown()).toBe(3);
+  });
+  test("[7] the SECOND CALL of counter.countDown returns the initial count minus one", () => {
+    counter.countDown();
+    expect(counter.countDown()).toBe(2);
+  });
+  test("[8] the count eventually reaches zero but does not go below zero", () => {
+    counter.countDown();
+    counter.countDown();
+    counter.countDown();
+    counter.countDown();
+    expect(counter.countDown()).toBe(0);
+  });
 });
 
 describe("[Exercise 5] Seasons", () => {
   let seasons;
   beforeEach(() => {
-    seasons = new utils.Seasons(); // each test must start with fresh seasons
+    seasons = new utils.Seasons();
   });
-  // test('[9] the FIRST call of seasons.next returns "summer"', () => {})
-  // test('[10] the SECOND call of seasons.next returns "fall"', () => {})
-  // test('[11] the THIRD call of seasons.next returns "winter"', () => {})
-  // test('[12] the FOURTH call of seasons.next returns "spring"', () => {})
-  // test('[13] the FIFTH call of seasons.next returns again "summer"', () => {})
-  // test('[14] the 40th call of seasons.next returns "spring"', () => {})
+  test("[9] the FIRST call of seasons.next returns 'summer'", () => {
+    expect(seasons.next()).toBe("summer");
+  });
+  test("[10] the SECOND call of seasons.next returns 'fall'", () => {
+    seasons.next();
+    expect(seasons.next()).toBe("fall");
+  });
+  test("[11] the THIRD call of seasons.next returns 'winter'", () => {
+    seasons.next();
+    seasons.next();
+    expect(seasons.next()).toBe("winter");
+  });
+  test("[12] the FOURTH call of seasons.next returns 'spring'", () => {
+    seasons.next();
+    seasons.next();
+    seasons.next();
+    expect(seasons.next()).toBe("spring");
+  });
+  test("[13] the FIFTH call of seasons.next returns 'summer'", () => {
+    seasons.next();
+    seasons.next();
+    seasons.next();
+    seasons.next();
+    expect(seasons.next()).toBe("summer");
+  });
+  test("[14] the 40th call of seasons.next returns 'spring'", () => {
+    for (let i = 0; i < 39; i++) {
+      seasons.next();
+    }
+    expect(seasons.next()).toBe("spring");
+  });
 });
 
 describe("[Exercise 6] Car", () => {
-  let focus;
+  let acura;
   beforeEach(() => {
-    focus = new utils.Car("focus", 20, 30); // each test must start with a fresh car
+    acura = new utils.Car("acura", 20, 20);
   });
-  // test('[15] driving the car returns the updated odometer', () => {})
-  // test('[16] driving the car uses gas', () => {})
-  // test('[17] refueling allows to keep driving', () => {})
-  // test('[18] adding fuel to a full tank has no effect', () => {})
+  test("[15] driving the car returns the updated odometer", () => {
+    acura.drive(50);
+    acura.drive(100);
+    acura.drive(100);
+    expect(acura.drive(50)).toBe(300);
+  });
+  test("[16] driving the car uses gas", () => {
+    expect(acura.tank).toBe(20);
+    acura.drive(100);
+    expect(acura.tank).toBeLessThan(20);
+  });
+  test("[17] refueling allows to keep driving", () => {
+    acura.drive(400);
+    expect(acura.tank).toBe(0);
+    expect(acura.drive(400)).toBe("You ran out of fuel at 400 miles");
+    expect(acura.refuel(20)).toBe(400);
+    expect(acura.drive(40)).toBe(440);
+    expect(acura.drive(400)).toBe(
+      "You ran out of gas while driving 400 miles! You made it to mile 360. Odometer is at 800"
+    );
+  });
+  test("[18] adding fuel to a full tank has no effect", () => {
+    expect(acura.tank).toBe(20);
+    expect(acura.refuel(1)).toBe("600 (tank only holds 20)");
+    expect(acura.tank).toBe(20);
+  });
 });
 
 describe("[Exercise 7] isEvenNumberAsync", () => {
-  // test('[19] resolves true if passed an even number', () => {})
-  // test('[20] resolves false if passed an odd number', () => {})
+  test("[19] resolves true if passed an even number", async () => {
+    const result = await utils.isEvenNumberAsync(2);
+    expect(result).toBe(true);
+  });
+  test("[20] resolves false if passed an odd number", async () => {
+    const result = await utils.isEvenNumberAsync(1);
+    expect(result).toBe(false);
+  });
 });
